2874a192370e525c235d2a49de0db14b3d6a1fb4
Ekow Turkson <eturkson@mofep.gov.gh>
2025-01-31 14:13:31 +0000

    Changing title name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@ git push -u origin main<!DOCTYPE html>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel 2</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
